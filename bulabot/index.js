d66e1554edbcb2eef6eab8625eea9373bab84689
Vitor Henckel <vitor@henckel.com.br>
2018-08-03 15:38:01 -0300

    Separando a regra de negócio do bot

--- bulabot/index.js
+++ bulabot/index.js
@@ -3,29 +3,7 @@ const { createServer } = require('bottender/express');
 
 const config = require('./bottender.config.js').messenger;
 
-const bot = new MessengerBot({
-  accessToken: config.accessToken,
-  appSecret: config.appSecret,
-});
-
-bot.onEvent(async context => {
-  await context.sendText('Hello World!');
-  if (context.event.isText) {
-    switch (context.event.text) {
-      case 'start':
-        await context.sendText('Running....');
-        break;
-      case 'help':
-        await context.sendText(`
-start   start to run
-help    quick help on <command>
-        `);
-        break;
-      default:
-        await context.sendText(`${context.event.text} is not a valid command.`);
-    }
-  }
-});
+const bot = require('./app/bot.js');
 
 const server = createServer(bot, {
   verifyToken: config.verifyToken,
